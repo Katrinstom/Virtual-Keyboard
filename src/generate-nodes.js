64dfd549fcbf6e3ad4c9b7b1a5bdec37007580fe
Ekaterina Stom <ekaterinastom@gmail.com>
2022-05-09 00:54:22 +0300

    feat: add keydown event

--- src/generate-nodes.js
+++ src/generate-nodes.js
@@ -19,135 +19,168 @@ keyboard.appendChild(keyboard__container);
 
 const description1 = document.createElement('p');
 description1.classList.add('description');
-description1.innerText = "Keyboard designed on macOs";
+description1.innerText = 'Keyboard designed on macOs';
 body.appendChild(description1);
 
 const description2 = document.createElement('p');
 description2.classList.add('description');
-description2.innerText = "To switch language En/Ru press Ctrl+Space";
+description2.innerText = 'To switch language En/Ru press Ctrl+Space';
 body.appendChild(description2);
 
+const arr = [
+    { code: 'Backquote', lang: { en: '>', ru: '§' } },
+    { code: 'Digit1', lang: { en: '1', ru: '1' } },
+    { code: 'Digit2', lang: { en: '2', ru: '2' } },
+    { code: 'Digit3', lang: { en: '3', ru: '3' } },
+    { code: 'Digit4', lang: { en: '4', ru: '4' } },
+    { code: 'Digit5', lang: { en: '5', ru: '5' } },
+    { code: 'Digit6', lang: { en: '6', ru: '6' } },
+    { code: 'Digit7', lang: { en: '7', ru: '7' } },
+    { code: 'Digit8', lang: { en: '8', ru: '8' } },
+    { code: 'Digit9', lang: { en: '9', ru: '9' } },
+    { code: 'Digit0', lang: { en: '0', ru: '0' } },
+    { code: 'Minus', lang: { en: '-', ru: '-' } },
+    { code: 'Equal', lang: { en: '=', ru: '=' } },
+    { code: 'Backspace', lang: { en: '⌫ Back', ru: '⌫ Back' } },
+    { code: 'Tab', lang: { en: 'tab', ru: 'tab' } },
+    { code: 'KeyQ', lang: { en: 'q', ru: 'й' } },
+    { code: 'KeyW', lang: { en: 'w', ru: 'ц' } },
+    { code: 'KeyE', lang: { en: 'e', ru: 'у' } },
+    { code: 'KeyR', lang: { en: 'r', ru: 'к' } },
+    { code: 'KeyT', lang: { en: 't', ru: 'е' } },
+    { code: 'KeyY', lang: { en: 'y', ru: 'н' } },
+    { code: 'KeyU', lang: { en: 'u', ru: 'г' } },
+    { code: 'KeyI', lang: { en: 'i', ru: 'ш' } },
+    { code: 'KeyO', lang: { en: 'o', ru: 'щ' } },
+    { code: 'KeyP', lang: { en: 'p', ru: 'з' } },
+    { code: 'BracketLeft', lang: { en: '[', ru: 'х' } },
+    { code: 'BracketRight', lang: { en: ']', ru: 'ъ' } },
+    { code: 'CapsLock', lang: { en: 'caps', ru: 'caps' } },
+    { code: 'KeyA', lang: { en: 'a', ru: 'ф' } },
+    { code: 'KeyS', lang: { en: 's', ru: 'ы' } },
+    { code: 'KeyD', lang: { en: 'd', ru: 'в' } },
+    { code: 'KeyF', lang: { en: 'f', ru: 'а' } },
+    { code: 'KeyG', lang: { en: 'g', ru: 'п' } },
+    { code: 'KeyH', lang: { en: 'h', ru: 'р' } },
+    { code: 'KeyJ', lang: { en: 'j', ru: 'о' } },
+    { code: 'KeyK', lang: { en: 'k', ru: 'л' } },
+    { code: 'KeyL', lang: { en: 'l', ru: 'д' } },
+    { code: 'Semicolon', lang: { en: ';', ru: 'ж' } },
+    { code: 'Quote', lang: { en: "'", ru: 'э' } },
+    { code: 'Enter', lang: { en: 'enter', ru: 'enter' } },
+    { code: 'ShiftLeft', lang: { en: 'shift', ru: 'shift' } },
+    { code: 'KeyZ', lang: { en: 'z', ru: 'я' } },
+    { code: 'KeyX', lang: { en: 'x', ru: 'ч' } },
+    { code: 'KeyC', lang: { en: 'c', ru: 'с' } },
+    { code: 'KeyV', lang: { en: 'v', ru: 'м' } },
+    { code: 'KeyB', lang: { en: 'b', ru: 'и' } },
+    { code: 'KeyN', lang: { en: 'n', ru: 'т' } },
+    { code: 'KeyM', lang: { en: 'm', ru: 'ь' } },
+    { code: 'Comma', lang: { en: ',', ru: 'б' } },
+    { code: 'Period', lang: { en: '.', ru: 'ю' } },
+    { code: 'Slash', lang: { en: '/', ru: '/' } },
+    { code: 'ArrowUp', lang: { en: '▲', ru: '▲' } },
+    { code: 'ShiftRight', lang: { en: 'shift', ru: 'shift' } },
+    { code: 'Fn', lang: { en: 'fn', ru: 'fn' } },
+    { code: 'ControlLeft', lang: { en: 'ctrl', ru: 'сtrl' } },
+    { code: 'AltLeft', lang: { en: 'opt', ru: 'opt' } },
+    { code: 'MetaLeft', lang: { en: 'cmd', ru: 'cmd' } },
+    { code: 'Space', lang: { en: 'space', ru: 'space' } },
+    { code: 'MetaRight', lang: { en: 'cmd', ru: 'cmd' } },
+    { code: 'ArrowLeft', lang: { en: '◄', ru: '◄' } },
+    { code: 'ArrowDown', lang: { en: '▼', ru: '▼' } },
+    { code: 'ArrowRight', lang: { en: '►', ru: '►' } },
+    { code: 'AltRight', lang: { en: 'opt', ru: 'opt' } },
+];
+let lang = 'rus';
+let capslock = false;
 
-const KeyboardKeys = {
-    constructor(name) {
-        this.name = name;
-    },
-    create() {
-        const arr = [
-            '>',
-            '1',
-            '2',
-            '3',
-            '4',
-            '5',
-            '6',
-            '7',
-            '8',
-            '9',
-            '0',
-            '-',
-            '=',
-            'del',
-            'tab',
-            'q',
-            'w',
-            'e',
-            'r',
-            't',
-            'y',
-            'u',
-            'i',
-            'o',
-            'p',
-            '[',
-            ']',
-            'caps',
-            'a',
-            's',
-            'd',
-            'f',
-            'g',
-            'h',
-            'j',
-            'k',
-            'l',
-            ';',
-            "'",
-            'enter',
-            'shift',
-            'z',
-            'x',
-            'c',
-            'v',
-            'b',
-            'n',
-            'm',
-            ',',
-            '.',
-            '/',
-            '▲',
-            'shift',
-            'fn',
-            'ctrl',
-            'opt',
-            'cmd',
-            'backspace',
-            'cmd',
-            '◄',
-            '▲',
-            '►',
-            'opt'
-        ];
-
+const KeyboardKeys = (arr) => {
         const createKeyName = (keyName) => {
             return `<div class="keyboard__key">${keyName}</div>`;
         };
-
         arr.forEach((el) => {
             const buttonKey = document.createElement('button');
             buttonKey.setAttribute('type', 'button');
 
-            if ((el.length == 1 || el == 'fn' || el == 'ctrl' || el == 'opt')) {
+            if (el.lang.en.length == 1 || el.lang.en == 'fn' || el.lang.en == 'ctrl' || el.lang.en == 'opt') {
                 buttonKey.classList.add('keyboard__button');
-                buttonKey.innerHTML = createKeyName(el);
+                buttonKey.setAttribute('id', el.code);
+                buttonKey.innerHTML = createKeyName(el.lang.en);
             } else {
-                switch (el) {
-                    case 'del':
+                switch (el.lang.en) {
+                    case '⌫ Back':
                         buttonKey.classList.add('keyboard__button-wide');
-                        buttonKey.innerHTML = createKeyName('del');
+                        buttonKey.setAttribute('id', el.code);
+                        buttonKey.innerHTML = createKeyName('⌫ Back');
                         break;
 
                     case 'tab':
                         buttonKey.classList.add('keyboard__button-wide');
+                        buttonKey.setAttribute('id', el.code);
                         buttonKey.innerHTML = createKeyName('tab');
                         break;
                     case 'caps':
-                        buttonKey.classList.add('keyboard__button-wide');
+                        buttonKey.classList.add('keyboard__button-wide', 'keyboard__button-caps');
+                        buttonKey.setAttribute('id', el.code);
                         buttonKey.innerHTML = createKeyName('caps');
+                        // buttonKey.addEventListener("keydown", () => {
+                        //     buttonKey.classList.toggle('keyboard__button-caps-active');
+                        // });
                         break;
                     case 'enter':
                         buttonKey.classList.add('keyboard__button-wide');
+                        buttonKey.setAttribute('id', el.code);
                         buttonKey.innerHTML = createKeyName('enter');
                         break;
                     case 'shift':
                         buttonKey.classList.add('keyboard__button-wide');
+                        buttonKey.setAttribute('id', el.code);
                         buttonKey.innerHTML = createKeyName('shift');
                         break;
                     case 'cmd':
                         buttonKey.classList.add('keyboard__button-wide');
+                        buttonKey.setAttribute('id', el.code);
                         buttonKey.innerHTML = createKeyName('cmd');
                         break;
-                    case 'backspace':
+                    case 'space':
                         buttonKey.classList.add('keyboard__button-super-wide');
-                        buttonKey.innerHTML = createKeyName('backspace');
+                        buttonKey.setAttribute('id', el.code);
+                        buttonKey.innerHTML = createKeyName('space');
                         break;
                 }
             }
             keyboard__container.appendChild(buttonKey);
         });
-    },
 };
+KeyboardKeys(arr);
 
-window.addEventListener('DOMContentLoaded', () => {
-    KeyboardKeys.create();
+
+const arrFromButtons = Array.from(document.querySelectorAll('button'));
+const textarea = document.querySelector('textarea').focus();
+console.log(arrFromButtons);
+
+document.addEventListener('keydown', (e) => { 
+    for(let elem of arrFromButtons) {
+        if(elem.id == e.code)  elem.classList.add('active');
+        if(arrFromButtons[27].id == e.code) arrFromButtons[27].classList.add('keyboard__button-caps-active');
+    }
 });
+document.addEventListener('keyup', (e) => { 
+    for(let elem of arrFromButtons) {
+        if(elem.id == e.code)  elem.classList.remove('active');
+        if(arrFromButtons[27].id == e.code) arrFromButtons[27].classList.remove('keyboard__button-caps-active');
+    }
+});
+
+
+
+
+
+// function addActive(elem) {
+//     elem.classList.add('active');
+// }
+
+// function removeActive(elem) {
+//     elem.classList.remove('active');
+// }
